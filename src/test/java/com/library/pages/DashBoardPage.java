79f3843beaf39561225e728aa87fd766d2197c0e
Nat <nattayasdet@gmail.com>
2022-04-28 16:42:52 -0400

    US02 finest commit.

--- src/test/java/com/library/pages/DashBoardPage.java
+++ src/test/java/com/library/pages/DashBoardPage.java
@@ -3,18 +3,23 @@ package com.library.pages;
 import com.library.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DashBoardPage {
-    public DashBoardPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public DashBoardPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    public void clickToAnyModule(String modName){
 
-        modName=modName.toLowerCase();
+    @FindBy(id = "borrowed_books")
+    public WebElement borrowedBooksNumber;
 
-        WebElement locateAnyModule=Driver.getDriver().findElement(By.xpath("//li[@class='nav-item']//a[@href='#"+modName+"']"));
+    public void clickToAnyModule(String modName) {
+
+        modName = modName.toLowerCase();
+
+        WebElement locateAnyModule = Driver.getDriver().findElement(By.xpath("//li[@class='nav-item']//a[@href='#" + modName + "']"));
         locateAnyModule.click();
 
     }
